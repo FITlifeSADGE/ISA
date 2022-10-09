2a2d617bffeb11c7c729a39f3ae4ee3c263f3685
FITlifeSADGE <lukas8967@gmail.com>
2022-10-09 19:48:06 +0200

    Added data extraction from TCP, UDP and ICMP packets

--- flow.c
+++ flow.c
@@ -1,12 +1,105 @@
 #include "argparse.h"
-#include <pcap.h>
+
+/*void hostname_ip(char **collector)
+{
+    int contains_char = 0;
+    char *IPbuffer = NULL;
+    for (size_t i = 0; i < strlen(*collector); i++)
+    {
+        if (isalpha(*collector[i]))
+        {
+            contains_char = 1;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if (!contains_char)
+    {
+        return;
+    }
+    struct hostent *host_entry = gethostbyname(*collector);
+    if ((IPbuffer = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0]))))
+    {
+        *collector = IPbuffer;
+    }
+    else
+    {
+        exit(1);
+    }
+}*/
+
+/*
+ * dissect/print packet
+ */
+void pcap_handle(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+
+    const struct sniff_ip *ip;   /* The IP header */
+    const struct sniff_tcp *tcp; /* The TCP header */
+    const struct sniff_udp *udp; /* The UDP header */
+
+    // time
+    struct tm ts;
+    char buf[80];
+
+    (void)args;
+    (void)header;
+
+    int size_ip;
+
+    /* define/compute ip header offset */
+    ip = (struct sniff_ip *)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip) * 4;
+
+    /* print source and destination IP addresses */
+    printf("   From: %s\n", inet_ntoa(ip->ip_src));
+    printf("   To: %s\n", inet_ntoa(ip->ip_dst));
+
+    /* ToS print */
+    printf("   ToS: %d\n", ip->ip_tos);
+
+    /* determine protocol */
+    switch (ip->ip_p)
+    {
+    case IPPROTO_TCP:
+        printf("   Protocol: TCP\n");
+        tcp = (struct sniff_tcp *)(packet + SIZE_ETHERNET + size_ip);
+        printf("   Src port: %d\n", ntohs(tcp->th_sport));
+        printf("   Dst port: %d\n", ntohs(tcp->th_dport));
+        break;
+    case IPPROTO_UDP:
+        printf("   Protocol: UDP\n");
+        udp = (struct sniff_udp *)(packet + SIZE_ETHERNET + size_ip);
+        printf("    Source port: %d\n", ntohs(udp->udp_sport));
+        printf("    Destination port: %d\n", ntohs(udp->udp_dport));
+        break;
+    case IPPROTO_ICMP:
+        printf("   Protocol: ICMP\n");
+        break;
+    default:
+        printf("   Protocol: unknown\n");
+        return;
+    }
+
+
+    // time print
+    printf("   Time: %ld\n", header->ts.tv_sec);
+    ts = *localtime(&header->ts.tv_sec);
+    strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", &ts);
+    printf("   Time: %s\n", buf);
+    return;
+}
 
 int device_set(char *file)
 {
     char errbuff[PCAP_ERRBUF_SIZE];
-    const u_char *packet;
     pcap_t *handle;
-    struct pcap_pkthdr header;
+    char filter[] = "ip";
+    bpf_u_int32 net = 0;
+    struct bpf_program fp;
     if (strcmp(file, "stdin") == 0)
     {
         if (!(handle = pcap_open_offline("-", errbuff)))
@@ -23,8 +116,19 @@ int device_set(char *file)
             return 1;
         }
     }
-    packet = pcap_next(handle, &header);
-    printf("Jacked a packet with length of %d\n", header.len);
+    if (pcap_compile(handle, &fp, filter, 0, net) == -1)
+    {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+    }
+    /* apply the compiled filter */
+    if (pcap_setfilter(handle, &fp) == -1)
+    {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter, pcap_geterr(handle));
+        exit(EXIT_FAILURE);
+    }
+    pcap_loop(handle, 10000, pcap_handle, NULL);
+    pcap_freecode(&fp);
     pcap_close(handle);
     printf("success\n");
     return 0;
@@ -52,5 +156,8 @@ int main(int argc, char **argv)
     {
         return 1;
     }
+    // struct hostent *host_entry = gethostbyname(collector);
+    // char *IPbuffer = inet_ntoa(*((struct in_addr *)host_entry->h_addr_list[0]));
+    // printf("%s\n", IPbuffer);
     return 0;
 }
